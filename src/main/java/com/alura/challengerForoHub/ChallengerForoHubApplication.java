3d3015e9136485a1e6aa2381618b3857ac3d1a92
JonatanR <jonatanjr-2017@outlook.es>
2024-07-16 23:14:01 -0500

    proyecto alura

---
+++ src/main/java/com/alura/challengerForoHub/ChallengerForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challengerForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challengerForoHub/ChallengerForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challengerForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
